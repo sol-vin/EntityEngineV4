8c3a9d8321da756769486e0435cb007929f27933
Ian Rash <redcodefinal@gmail.com>
2013-08-09 01:51:08 -0700

    Added Shufflebag

---
+++ EntityEngineV4/Data/Collections/Shufflebag.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntityEngineV4.Data.Collections
+{
+    public class Shufflebag<T> : List<T>
+    {
+        public List<T> CurrentItems { get; private set; }
+        private Random _random = new Random();
+
+        public Shufflebag()
+        {
+            CurrentItems = new List<T>();
+        }
+
+        public T GetNextItem()
+        {
+            if (CurrentItems.Count == 0)
+            {
+                Reset();
+            }
+
+            T item = CurrentItems[_random.Next(0, CurrentItems.Count)];
+            CurrentItems.Remove(item);
+            return item;
+        }
+
+        public void Reset()
+        {
+            CurrentItems = this.ToList();
+        }
+    }
+}
